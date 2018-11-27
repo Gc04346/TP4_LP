78109afcdefc1b0b5c450e7bbd566241fbb905aa
Daniel Santana Santos <gc04346@gmail.com>
2018-11-27 18:41:37 +0000

    Update DanielSantana_IsaqueFernando.pl

--- DanielSantana_IsaqueFernando.pl
+++ DanielSantana_IsaqueFernando.pl
@@ -3,3 +3,12 @@ nelementos([],0).
 nelementos([_|L],Tam) :- nelementos(L,Tam1), Tam is Tam1+1.
 
 % 2
+maior([X|L],M) :- maior2(L, X, M).
+maior2([],M,M).
+maior2([X|L], T, M) :- X > T, maior2(L, X, M).
+maior2([X|L], T, M) :- X =< T, maior2(L, T, M).
+
+%3
+media([X|L],M) :- soma(L, X, 1, M).
+soma([], S, C, M) :- M is S / C.
+soma([X|L], S, C, M) :- S is X+S1, C is Contador+1, soma(L, S1, Contador, M).
