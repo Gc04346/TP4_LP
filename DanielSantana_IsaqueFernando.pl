0f6ac606fb5ac5c252603738a7d612d6bc52505b
Isaque Fernando <isaquefernando10@gmail.com>
2018-11-30 17:30:02 +0000

    Update DanielSantana_IsaqueFernando.pl

--- DanielSantana_IsaqueFernando.pl
+++ DanielSantana_IsaqueFernando.pl
@@ -8,7 +8,19 @@ maior2([],M,M).
 maior2([X|L], T, M) :- X > T, maior2(L, X, M).
 maior2([X|L], T, M) :- X =< T, maior2(L, T, M).
 
-%3
+%% Terceiro exercicio. Media de dois números.
 media([X|L],M) :- soma(L, X, 1, M).
 soma([], S, C, M) :- M is S / C.
 soma([X|L], S, C, M) :- S is X+S1, C is Contador+1, soma(L, S1, Contador, M).
+
+%% Quarto exercicio. Inserir elemento no final da lista
+%% Função para obter o tamanho de uma lista.
+tamLista([],0).
+tamLista([_|L],S) :- tamLista(L,N), S is N+1.
+
+%% Soma dos elementos da lista
+somaElementos([], 0).
+somaElementos([X|L], S) :- somaElementos(L, M), S is M+X.
+
+%% Media de ums lista.
+media(X,M) :- tamLista(X,N), somaElementos(X,Y), M is Y/N. 
