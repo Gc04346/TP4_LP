b2db1b3d05055408627c05c6a36bd48118bb8856
Isaque Fernando <isaquefernando10@gmail.com>
2018-11-30 18:08:26 +0000

    Update DanielSantana_IsaqueFernando.pl

--- DanielSantana_IsaqueFernando.pl
+++ DanielSantana_IsaqueFernando.pl
@@ -1,19 +1,14 @@
-% 1
+% Primeiro exercício. Numero de elementos na lista.
 nelementos([],0).
 nelementos([_|L],Tam) :- nelementos(L,Tam1), Tam is Tam1+1.
 
-% 2
+% Segundo exercício. Maior elemento da lista.
 maior([X|L],M) :- maior2(L, X, M).
 maior2([],M,M).
 maior2([X|L], T, M) :- X > T, maior2(L, X, M).
 maior2([X|L], T, M) :- X =< T, maior2(L, T, M).
 
 %% Terceiro exercicio. Media de dois números.
-media([X|L],M) :- soma(L, X, 1, M).
-soma([], S, C, M) :- M is S / C.
-soma([X|L], S, C, M) :- S is X+S1, C is Contador+1, soma(L, S1, Contador, M).
-
-%% Quarto exercicio. Inserir elemento no final da lista
 %% Função para obter o tamanho de uma lista.
 tamLista([],0).
 tamLista([_|L],S) :- tamLista(L,N), S is N+1.
@@ -24,3 +19,8 @@ somaElementos([X|L], S) :- somaElementos(L, M), S is M+X.
 
 %% Media de ums lista.
 media(X,M) :- tamLista(X,N), somaElementos(X,Y), M is Y/N. 
+
+%% Quarto exercicio. Inserir elemento no final da lista
+%% Quarto exercício. Inserir elemento no final da lista.
+inserirfim(Y, [], [Y]).
+inserirfim(Y,[X|L], M) :- inserirfim(Y,L,W), M = [X|W].
